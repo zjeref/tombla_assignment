b2361c62b94bbffc3ed49dc8214ea381eed9a6cc
Ram Kumar <ramkr0072@gmail.com>
2023-06-03 14:12:18 +0530

    added basic comments

--- routes/ticket-routes.js
+++ routes/ticket-routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const Router = express.Router();
 const {createTicket, fetchTicket} = require('../controller/ticket-controller')
 
-Router.post('/create', createTicket)
-Router.get('/fetch', fetchTicket)
-
+Router.post('/create', createTicket)  //   /api/ticket/create?number=2
+Router.get('/fetch', fetchTicket) //   /api/ticket/fetch?currentPage=2&pageSize=3
+ 
 
 module.exports = Router
